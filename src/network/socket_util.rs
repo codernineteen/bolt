4deb2eebd4ebbf5ff649200d258d439687095e4f
codernineteen <wjsdPcks34@gmail.com>
2023-08-08 17:11:11 +0900

    Removed unused module 'socket utils'

--- src/network/socket_util.rs
+++
@@ -1,7 +0,0 @@
-// use {super::net_addr::NetAddress, tokio::net::TcpListener};
-
-// pub async fn test() {
-//     let listener_try = TcpListener::bind("127.0.0.1:8080").await;
-//     let listener = listener_try.unwrap();
-//     listener.
-// }
